8b40592f8e50e02a5ae4c3b5f15af25f942e296c
george <george@parity.io>
2023-07-04 16:24:28 +0100

    modelling changes

--- src/components/CanvasContainer.tsx
+++ src/components/CanvasContainer.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import { JordanShoes } from './JordanShoes';
-import { OrbitControls } from '@react-three/drei';
+import { Environment, OrbitControls } from '@react-three/drei';
 
 export const CanvasContainer = () => {
     return (
@@ -9,9 +9,9 @@ export const CanvasContainer = () => {
 					4.742922067308307, 2.2387122409413784, 1.2218255872664914,
 				],
 			}}>
-                <ambientLight/>
                 <OrbitControls/>
                 <JordanShoes/>
+                <Environment preset="city"/>
             </Canvas>
     );
 }
--- src/components/JordanShoes.tsx
+++ src/components/JordanShoes.tsx
@@ -4,10 +4,10 @@ import { JordanGLTF } from "../typings";
 export const JordanShoes = () => {
     const {nodes, materials} = useGLTF("/public/air_jordan_1.glb") as JordanGLTF;
     return <>
-    <group scale={10} rotation={[-Math.PI * 0.5, 0, 0]} position={[2,1,-1]}>
-        <mesh geometry={nodes.shoe_shoe_0.geometry} material={materials.shoe} />
-        <mesh geometry={nodes.shoelace_shoelace_0.geometry} material={materials.shoelace} />
-    </group>
+        <directionalLight castShadow position={[-2.38, 1.32, 0.74]} />
+        <group scale={10} rotation={[-Math.PI * 0.5, 0, 0]} position={[2,1,-1]}>
+            <mesh geometry={nodes.shoe_shoe_0.geometry} material={materials.shoe} />
+            <mesh geometry={nodes.shoelace_shoelace_0.geometry} material={materials.shoelace} />
+        </group>
     </>;
 }
- 
